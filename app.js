2478b17a47802f5a161c2082836ac89e796466c3
mmarque <marquezmusa@gmail.com>
2024-08-17 17:46:10 -0400

    code for Fire button

--- app.js
+++ app.js
@@ -8,25 +8,23 @@ There are six alien ships. The aliens' weakness is that they are too logical and
 */
 let round = 0;
 
+// ----------[]
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 class GameCharacter {
   constructor(name, hull, firepower, accuracy) {
     this.name = name;
     this.hull = hull;
     this.firepower = firepower;
     this.accuracy = accuracy;
+    /*toggle */
+    this.ussAssemblyTurn = true;
+    this.alienTurn = false;
 
     // this.attack = this.attack.bind(this); // Binding 'constructor' to the this
   }
-  /* attack ability */
-  attack = () => {
-    const randomNum = randomNumber(); // ?move the random here
-    console.log(this.randomNum);
-    return randomNum;
-  };
-  /* Random number generator */
-  randomNumber = () => {
-    return Math.floor(Math.random() * 10) + 1;
-  };
 }
 
 // create an alien
@@ -44,28 +42,108 @@ class EarthDefender extends GameCharacter {
   }
 }
 
-/*create characters  the 2 Aliens*/
-AlienMini = new Alien("AlienMini", 3, 5, 0.5);
-AlienMotherShip = new Alien("AlienMotherShip", 6, 4, 0.7);
+/*create characters: name, hull,firepower,accuracy*/
+
+alienOne = new Alien(
+  "miniAlien",
+  generateRandomNumber(3, 6),
+  generateRandomNumber(2, 4),
+  generateRandomNumber(0.6, 0.8)
+);
 
 /*create the EarthDefender Player */
-USSpaceForce = new EarthDefender("USSpaceForce", 20, 5, 0.7);
+ussAssembly = new EarthDefender("ussAssembly", 20, 5, 0.7);
 
-/* create a start function*/
-function start() {
+/*-----------------------------------*/
+/* ----- Game Start function --------*/
+/* USSASSEMBLY ussAssemblyTurn=true  */
+/* ALIEN       alienTurn=false       */
+/*-----------------------------------*/
+document.querySelector("#start").addEventListener("click", function () {
   console.log("start");
-  const fightRound = document.querySelector(".fightRound");
-  round++;
-  fightRound.innerHTML = round;
-}
 
+  const fightRound = document.querySelector(".fightRound");
+  round = 1;
+  fightRound.innerHTML = round;
+  /* Load statistics*/
+  document.querySelector("#alienHull").innerHTML = `${alienOne.hull}`;
+  document.querySelector("#alienFire").innerHTML = `${alienOne.firepower}`;
+  document.querySelector("#alienAccuracy").innerHTML = `${alienOne.accuracy}`;
+  document.querySelector("#playerHull").innerHTML = `${USSpaceForce.hull}`;
+  document.querySelector("#playerFire").innerHTML = `${USSpaceForce.firepower}`;
+  document.querySelector(
+    "#playerAccuracy"
+  ).innerHTML = `${USSpaceForce.accuracy}`;
+});
+
+/*--------------------------------*/
+/* ----- Game Fire function -----*/
+/*--------------------------------*/
+document.querySelector("#fire").addEventListener("click", () => {
+  console.log("fire initiated");
+  console.log(`${ussAssembly.name} toggle is ${ussAssembly.ussAssemblyTurn}`);
+
+  ussAssembly.ussAssemblyTurn
+    ? calculateStatistics(ussAssembly.name)
+    : calculateStatistics(alienOne.name); // ternary operator ( ===)
+});
+
+/*--------- calculate statistics        ------------------- */
+
+calculateStatistics = (name) => {
+  const randomMath = Math.floor(Math.random() * 10) / 10;
+  console.log(randomMath);
+  switch (name) {
+    case ussAssembly.name:
+      if (randomMath < `${name}`.accuracy) {
+        console.log(`${name} is hit`);
+        /*reduce the alien's hull*/
+        alienOne.hull -= 1;
+        document.querySelector("#alienHull").innerHTML = `${alienOne.hull}`;
+        console.log("hit");
+      } else {
+        console.log("missed");
+      }
+      break;
+    case alienOne.name:
+      if (randomMath < ussAssembly.accuracy) {
+        console.log("${name} is hit");
+        /*reduce the player's hull*/
+        alienOne.hull -= 1;
+        document.querySelector("#playerHull").innerHTML = `${alienOne.hull}`;
+      } else {
+        console.log("missed");
+      }
+      break;
+  }
+};
+/*----------return name and toggle turn ------------------- */
+toggleTurn = () => {
+  switch (true) {
+    case "alienTurn":
+      console.log("alien turn");
+
+      break;
+  }
+  if (ussAssembly.ussAssemblyTurn) {
+    ussAssembly.ussAssemblyTurn = false; // object properties
+    alienOne.alienTurn = true;
+    return ussAssembly.name;
+  } else if (!ussAssemblyTurn) {
+    ussAssemblyTurn = true;
+    alienTurn = false;
+    return alienOne.name;
+  }
+};
 /* check child object properties/prototype*/
-console.log(Object.getOwnPropertyNames(AlienMini));
-console.log(Object.getPrototypeOf(AlienMotherShip));
+console.log(Object.getOwnPropertyNames(alienOne));
+console.log(Object.getPrototypeOf(alienOne));
 
 console.log(
-  `${AlienMotherShip.name} ${AlienMotherShip.hull} ${AlienMotherShip.firepower} ${AlienMotherShip.accuracy}`
+  `${alienOne.name} ${alienOne.hull} ${alienOne.firepower} ${alienOne.accuracy}`
 );
+
+/* set statistics */
 
 // const bird = document.querySelector(".bird");
 // let positionX = 0;
